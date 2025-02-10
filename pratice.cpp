95a2c875d30b7b8c0638e8e2e5127a926f82072b
Tejasai-S <tjg41699@gmail.com>
2025-02-10 21:10:58 +0530

    10/02/2025 9:10

--- pratice.cpp
+++ pratice.cpp
@@ -1,35 +1,16 @@
 /*#include <iostream>
-
 int main(){
     std::cout << "hello world" "\n";
     std::cout << "i am tejasai" "\n";
 }*/
+
+
 //declaration program
 /*#include <iostream>
-
 int main(){
     int x;
     x = 55;
     std::cout << "x:" << x << std::endl;
 }*/
-//simple data types
-#include <iostream>
 
-int main(){
-    //int
-    int x=55;
-    //string
-    std::string name = "rahul";
-    //chr
-    char grade ='A';
-    //double 
-    double percentage = 82.97;
-    //bollen
-    bool student = true;
 
-    std::cout << "int:" << x << "\n";
-    std::cout << "String:" << name << "\n";
-    std::cout << "char:" << grade << "\n";
-    std::cout << "double:" << percentage << "\n";
-    std::cout << "bollen:" << student << "\n";
-}
