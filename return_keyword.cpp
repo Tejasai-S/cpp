77662a952c83dc7a8a8d10af49b94aafae137936
Tejasai-S <tjg41699@gmail.com>
2025-02-20 00:27:20 +0530

    19/02/2025 11:50

--- return_keyword.cpp
+++ return_keyword.cpp
@@ -5,7 +5,7 @@ std::string fullname(std::string FN,std::string SN);
 int main(){
     std::string FN = "Tejasai";
     std::string SN = "S";
-    std::string Fullname = fullname(FN,SN);
+    std::string Fullname = fullname(FN,SN); 
 
     std::cout << "Fullname: " << Fullname << "\n";
     return 0;
